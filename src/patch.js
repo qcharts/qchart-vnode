314a7041955a010c28f7e349d63a66cef6402c16
yaotaiyang <yaotaiyang99@gmail.com>
2020-03-07 14:24:14 +0800

    bug fixed

--- src/patch.js
+++ src/patch.js
@@ -17,15 +17,14 @@ export function patchAttrs(graph, el, patche) {
 }
 
 /**
- * @param {*} parent
- * @param {*} patches
- * @param {*} index
+ * @param {*} $el 当前的真实spritejs 节点
+ * @param {*} patches 虚拟节点
+ * @param {*} isRoot 是否是初节点，用于绑定$el与visual或者plugin
  */
-export default function patch(parent, patche, num = 0) {
+export default function patch($el, patche, isRoot = 0) {
   //core中调用patch的时候，base中处理绑定this到当前图表
-  let i = num
-  let graph = this
-  if (!patche || !parent) {
+  let graph = this //graph为当前的qcharts对象 visula或者plugin
+  if (!$el || !$el) {
     return
   }
   switch (patche.type) {
@@ -33,36 +32,38 @@ export default function patch(parent, patche, num = 0) {
       const { newVNode } = patche
       const newEl = createElement.bind(graph)(newVNode)
       if (newEl) {
-        graph.$el = newEl
-        parent.appendChild(newEl)
+        if (isRoot) {
+          //创建的时候，visual与plugin绑定对应的sprite元素
+          graph.$el = newEl
+        }
+        $el.appendChild(newEl)
       }
       break
     }
     case REPLACE: {
       const { newVNode } = patche
       const newEl = createElement(newVNode)
-      parent.replaceChild(newEl, el)
+      $el.replaceChild(newEl, el)
       break
     }
     case REMOVE: {
-      parent.children[i] && parent.children[i].remove()
+      $el.remove()
       break
     }
     case UPDATE: {
       const { attrs, children } = patche
       //对应的dom与vnode打上对应的ind，防止remove与create的时候变动i
-      console.log('aa')
-      if (parent.children && children.length) {
-        parent.children.forEach((child, ind) => {
+      patchAttrs(graph, $el, attrs)
+      if ($el.children && children.length) {
+        $el.children.forEach((child, ind) => {
           child._ind = ind
         })
         children.forEach((child, ind) => {
           child._ind = ind
         })
-        let curNode = parent.children.filter(child => child._ind === i)[0]
-        patchAttrs(graph, curNode, attrs)
-        let len = children.length > parent.children.length ? children.length : parent.children.length
+        let len = children.length > $el.children.length ? children.length : $el.children.length
         for (let j = 0; j < len; j++) {
+          let curNode = $el.children.filter(child => child._ind === j)[0]
           patch.bind(graph)(curNode, children.filter(child => child._ind === j)[0], j)
         }
       }
